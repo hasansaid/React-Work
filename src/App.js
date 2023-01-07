f4218b7d13d8245927e74a0f9b8be605feb22de7
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-08 01:02:17 +0300

    adding proptypes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import Header from './components/header.jsx';
-import Login from './components/login.jsx';
+// import Header from './components/header.jsx';
+// import Login from './components/login.jsx';
 import User from './components/user.jsx';
 
 function App () {
-  const isLoggedIn = true;
-  const fullName = 'Hasan Said SERTKAYA';
+  // const isLoggedIn = true;
+  // const fullName = 'Hasan Said SERTKAYA';
+
+  const user = {
+    firstname: 'Hasan Said',
+    lastname: 'SERTKAYA',
+    email: 'hs@gmail.com',
+  };
 
   return (
     <div>
@@ -14,8 +20,12 @@ function App () {
         ? <div> <Header /> <hr /> Hoşgeldin {fullName} </div>
         : <Login />} */}
 
+      {/* PROPS */}
+      {/* 
       <User name="Hasan" surname="SERTKAYA" age={23} />
-      <User name="Said" surname="SERTKAYA" age={25} />
+      <User name="Said" surname="SERTKAYA" age={25} /> */}
+
+      <User data={user} friends={['Ali ', 'Ayşe ', 'Murat']} />
     </div>
   );
 }
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-
-function User(props) {
-  //   console.log(props);
+import PropTypes from "prop-types";
+function User({ data, friends }) {
+  // console.log(friends);
+  //Farklı kulanımlar..
+  // const data = props.data;
+  // const {data}=props;
+  //   const {firstname,lastname,email} = props.data;
   return (
     <div>
-      <table>
+      {/* <table>
         <tr>
           <td>Name: {props.name}</td>
           <br />
@@ -13,9 +17,52 @@ function User(props) {
           <td>Age: {props.age}</td>
         </tr>
       </table>
-      <hr />
+      <hr /> */}
+
+      {/* <table>
+        <tr>
+          <th>Name</th>
+          <br />
+          <th>Surname</th>
+          <br />
+          <th>Email</th>
+          <br />
+          <th>Friends</th>
+        </tr>
+        <tr>
+          <td>{data.firstname}</td>
+          <br />
+          <td> {data.lastname}</td>
+          <br />
+          <td> {data.email}</td>
+          <br />
+          <td>
+            {friends.map((friend, i) => (
+              <div key={i}>
+                {i + 1} - {friend}
+              </div>
+            ))}
+          </td>
+        </tr>
+      </table>
+      <div></div>
+      <hr /> */}
+
+      <p>First Name: {data.firstname}</p>
+      <p>Last Name: {data.lastname}</p>
+      <p>Email: {data.email}</p>
+      <p>Friends: {friends}</p>
     </div>
   );
 }
+
+User.propTypes = {
+  data: PropTypes.exact({
+    firstname: PropTypes.string,
+    lastname: PropTypes.string,
+    email: PropTypes.string,
+  }),
+  friends: PropTypes.array,
+};
 
 export default User;
