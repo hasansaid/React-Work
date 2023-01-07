5c1edc1c7f798a654802dcb9f317cbb72280efb8
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-07 20:55:41 +0300

    arrangement

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,19 @@
+//component ve jsx kuralları:
+//1 - Yazılan component çağırılırken ilk harfi büyük olmalıdır. Küçük harf ile yazıldığında html etiketi olarak algılar.
+//2 - Component dosya isminde ilk harfi küçük olabilir.
+//3 -
+
 function Header() {
+  // return React.createElement ('div', null, React.createElement ('p', null));
+
+  // return (
+  //   <div>
+  //     <p></p>
+  //   </div>
+  // );
+
+  // Yukarıda kullanılan return'lar aynı işlevi görmektedir. İkinci return kullanımı jsx ile hayatımıza girip bizi karmaşadan kurtardı.
+
   return (
     <div>
       <h1>Başlık</h1>
