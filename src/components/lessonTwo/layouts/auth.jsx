591f7e722d5e8714e004667b4d043d25b4b7cfbf
WinnerRey <64565872+hasansaid@users.noreply.github.com>
2023-01-11 23:59:48 +0300

    arrangement

--- src/components/lessonTwo/layouts/auth.jsx
+++ src/components/lessonTwo/layouts/auth.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import "./styles.css";
 
 const Auth = () => {
   return (
-    <div>
-      <div id="content">
-        <Outlet />
+    <div className="auth-container">
+      Auth
+      <div id="auth-menu">
+        <Link to="/auth">Giriş Yap</Link>
+        <Link to="/auth/register">Kayıt Ol</Link>
       </div>
     </div>
   );
